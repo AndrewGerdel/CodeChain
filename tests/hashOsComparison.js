5deded7372ea5dad1019d6bbd669917c6e2534fe
Andrew Gerdel <AndrewGerdel@yahoo.com>
2019-01-14 21:46:18 -0600

    logging

--- tests/hashOsComparison.js
+++ tests/hashOsComparison.js
@@ -5,7 +5,7 @@ var crypto = require('crypto');
 var nonce = 0;
 var effectiveDate = new Date('1/1/2000');
 var mempoolItems = [];
-var hashInput = nonce + effectiveDate.toISOString() + JSON.stringify(mempoolItems) + 'None';
+var hashInput = 'The Genesis Block';
 var hash = crypto.createHmac('sha256', hashInput).digest('hex');
 
 console.log('effectivedate string is', effectiveDate.toISOString() );
